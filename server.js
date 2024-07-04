71672c73911ac957140f8e0276b749ca19a2bf9e
Rajankumar001 <chaudharyrajan387@gmail.com>
2024-07-04 20:12:05 +0530

    allow cors origin

--- server.js
+++ server.js
@@ -15,18 +15,13 @@ const app=express();
 
 // middleware
 app.use(express.json());
-app.use(cors(
-  {
-    origin:"https://chaudhary-portfolio.netlify.app/",
-    optionsSuccessStatus: 200
-  }
- ));
- app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://chaudhary-portfolio.netlify.app/');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
+ app.use(cors({
+  origin: 'https://chaudhary-portfolio.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Content-Length', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Headers', 'Access-Control-Allow-Methods', 'Access-Control-Expose-Headers', 'Set-Cookie'],
+  }));
 
  app.use(express.urlencoded({extended:true}));
  app.use(express.static(path.join(__dirname,'./client/dist')));
