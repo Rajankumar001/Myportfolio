f397bccca37b5fef959f14d767b9aadd96c1609c
Rajankumar001 <chaudharyrajan387@gmail.com>
2024-07-04 16:39:59 +0530

    changes in toastify

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import './Contact.css';
 import contact_image from '../../assets/images/contact_img.jpg';
 import Axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const notify = () => toast("message sent successfully");
 const Contact = () => {
   const [name, setName]=useState(" ");
   const [email ,setEmail]=useState(" ");
@@ -14,7 +17,7 @@ const Contact = () => {
     }
     try {
       console.log("its working");
-        const result = await Axios.post('http://localhost:8080/portfolio/sendEmail', { name, email, message })
+        const result = await Axios.post('https://myportfolio-7.onrender.com/portfolio/sendEmail', { name, email, message })
         console.log("Data sent:", result.config.data); // Accessing sent data
 
         console.log("Message saved:", result.data.message); // Accessing response data
@@ -24,6 +27,7 @@ const Contact = () => {
         setName("");
         setEmail("");
         setMessage("");
+        notify();
       }
         catch (error) {
         console.log("Error caught:", error);
@@ -40,14 +44,14 @@ const Contact = () => {
                 <img src={contact_image} alt="" className='conatact_image_pic' />
             </div>
             <div class="mb-3 col-8 contact-form">
-             
+            <ToastContainer />  
   <label for="exampleFormControlInput1" class="form-label">Name</label>
   <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter your Name " value={name} onChange={(e)=>setName(e.target.value)}/>
   <label for="exampleFormControlInput1" class="form-label">Email</label>
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com " value={email} onChange={(e)=>setEmail(e.target.value)}/>
   <label for="exampleFormControlTextarea1" class="form-label">Message </label>
   <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder='Drop a Message here!' value={message} onChange={(e)=>setMessage(e.target.value)}></textarea>
-  <button className='btn btn-primary contact-button' onClick={handleform}>Send Message</button>
+  <button className='btn btn-primary contact-button' onClick={handleform }>Send Message</button>
  
 </div>
         </div>
