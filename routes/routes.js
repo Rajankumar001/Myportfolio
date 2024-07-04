3aeb827350cef6ab949b5a4bd510edcdd066ac5e
Rajankumar001 <chaudharyrajan387@gmail.com>
2024-07-04 19:26:12 +0530

    changes in link

--- routes/routes.js
+++ routes/routes.js
@@ -4,12 +4,12 @@ const express=require('express');
  const cors = require('cors');
  router.use(cors(
   {
-    origin:"http://localhost:5173",
+    origin:"https://chaudhary-portfolio.netlify.app/",
     optionsSuccessStatus: 200
   }
  ));
  router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://chaudhary-portfolio.netlify.app/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
